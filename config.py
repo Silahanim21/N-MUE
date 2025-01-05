5c3a7d8638f73b4198f1b5656de808b18daf1879
Silahanim21 <turkcell63632@gmail.com>
2025-01-05 21:38:21 +0300

    Update config.py

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-  "https://github.com/Silahanim21/D-SNEYMUS-C",
+  "https://github.com/Silahanim21/N-MUE",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
