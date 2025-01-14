ad2ca533aa8b39f9ea5bb99260a5304633936b6f
Silahanim21 <turkcell63632@gmail.com>
2025-01-14 22:44:38 +0300

    Update play.py

--- SYSTUM/utils/inline/play.py
+++ SYSTUM/utils/inline/play.py
@@ -69,7 +69,7 @@ def stream_markup_timer(_, chat_id, played, dur):
         ],
       [
             InlineKeyboardButton(
-                text="🧚 𝙁𝙀𝙔 𝙆𝙄𝙕𝙄 🧚", url="https://t.me/Owner_kumsal_team",            
+                text="🧚 sohbet grubu 🧚", url="https://t.me/PembeninKurku",            
             )
         ],
          [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
