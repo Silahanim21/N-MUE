54107c7051ba4fafea5f05e0d83ed93de07d733d
Silahanim21 <turkcell63632@gmail.com>
2025-01-14 23:04:39 +0300

    Update config.py

--- config.py
+++ config.py
@@ -90,10 +90,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", None
+    "START_IMG_URL", https://envs.sh/vst.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", None
+    "PING_IMG_URL", "https://envs.sh/vst.jpg"
 PLAYLIST_IMG_URL = "https://envs.sh/sFt.jpeg"
 STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
 TELEGRAM_AUDIO_URL = "https://envs.sh/sFt.jpeg"
